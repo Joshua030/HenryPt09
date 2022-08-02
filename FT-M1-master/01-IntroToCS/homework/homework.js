153928cf41b650d1399cb331304581a5b15c4582
Arq. Carolina Arias <joseluy123@gmail.com>
2022-08-02 01:44:24 -0500

    Second commit

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,12 +2,23 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  array = [];
+for(let i = num ; num > 1 ; num = num /2) {
+  let x = Math.floor(num % 2) ;
+  array.push (x);
 }
-
+return array.reverse().join("");
+}
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let  array = String(num).split("").reverse();
+  let count = 0.5 ;
+  let suma = 0;
+  for(let i = 0 ; i <array.length; i++) {
+    count *= 2 ;
+    suma += Number(array[i])* count;
+  }
+  return suma;
 }
 
 
