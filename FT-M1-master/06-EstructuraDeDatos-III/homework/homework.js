ae07f318457389bf3935ee8dd4ea1ec26b1a5b15
Arq. Carolina Arias <joseluy123@gmail.com>
2022-08-22 21:06:10 -0500

    Eighth commit

--- FT-M1-master/06-EstructuraDeDatos-III/homework/homework.js
+++ FT-M1-master/06-EstructuraDeDatos-III/homework/homework.js
@@ -10,8 +10,102 @@
 
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
+class BinarySearchTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 
-function BinarySearchTree() {}
+  // function to be implemented
+  // insert(data)
+  // remove(data)
+
+
+  // Helper function
+  // findMinNode()
+  // getRootNode()
+  // inorder(node)
+  // preorder(node)              
+  // postorder(node)
+  // search(node, data)
+  insert(value) {
+    if (value > this.value) {
+      if (this.right === null)
+        this.right = new BinarySearchTree(value)
+      else this.right.insert(value);
+    }
+
+    if (value < this.value) {
+      if (this.left === null)
+        this.left = new BinarySearchTree(value)
+      else this.left.insert(value);
+    }
+  }
+
+  size() {
+    if (this.value === null) return 0;
+    if (this.left === null && this.right === null) return 1;
+    if (this.left === null && this.right !== null) return 1 + this.right.size();
+    if (this.left !== null && this.right === null) return 1 + this.left.size();
+    return 1 + this.right.size() + this.left.size();
+
+  }
+  contains(value) {
+    if (this.value === value) return true;
+
+    if (value > this.value) {
+      if (this.right === null) return false;
+      return this.right.contains(value);
+    }
+    if (value < this.value) {
+      if (this.left === null) return false;
+      return this.left.contains(value);
+    }
+  }
+  depthFirstForEach(cb, traverse) {
+    if (traverse === "post-order") {
+      if (this.left !== null) this.left.depthFirstForEach(cb, traverse);
+      if (this.right !== null) this.right.depthFirstForEach(cb, traverse);
+      cb(this.value);
+
+    }
+    else if (traverse === "pre-order") {
+      cb(this.value)
+      if (this.left !== null) this.left.depthFirstForEach(cb, traverse);
+      if (this.right !== null) this.right.depthFirstForEach(cb, traverse);
+    }
+    else {
+      if (this.left !== null) this.left.depthFirstForEach(cb, traverse);
+      cb(this.value);
+      if (this.right !== null) this.right.depthFirstForEach(cb, traverse);
+    }
+  }
+  breadthFirstForEach(cb, queue = []) {
+    if (this.left !== null) queue.push(this.left);
+    if (this.right !== null) queue.push(this.right);
+
+    cb(this.value);
+
+    if (queue.length > 0) {
+      queue.shift().breadthFirstForEach(cb, queue);
+    }
+
+  }
+}
+
+
+
+
+//     // find the correct position in the
+//     // tree and add the node
+//     this.insertNode(this.value, node);
+
+
+
+
+
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
